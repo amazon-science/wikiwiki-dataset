d5fada0875fcfbba1cd87d283b3a9d6ed152cb51
Mukund Harakere-sridhar <harakere@amazon.com>
2022-04-28 14:45:41 -0400

    added bash script to download data

---
+++ data/download_data.sh
@@ -0,0 +1,4 @@
+wget https://wiki-wiki.s3.amazonaws.com/test_seen_entities.jsonl .
+wget https://wiki-wiki.s3.amazonaws.com/test_unseen_entities.jsonl .
+wget https://wiki-wiki.s3.amazonaws.com/glossary.tsv.zip .
+wget https://wiki-wiki.s3.amazonaws.com/train.jsonl.zip .
